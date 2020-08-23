1496394c39bae1ea44425bcfa66cf435251a8aad
molly mk <mmkover@gmail.com>
2020-08-23 19:02:53 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,8 @@ end
 def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday|
     holiday.each do |holiday, supplies|
-          
+      if supplies.include! ("BBQ")
+        puts holiday
 end
 
 
