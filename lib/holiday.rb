e7861d73c483110809d73f8516894d2338594c1f
molly mk <mmkover@gmail.com>
2020-08-23 18:20:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -32,7 +32,7 @@ end
 def add_supply_to_memorial_day(holiday_hash, supply)
   holiday_hash[:spring][:memorial_day] << supply
   holiday_hash
-  holiday_hash[:spring][:memorial_day] << "Table Cloth"
+  
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
