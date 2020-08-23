7999265d900e45a6bc267b99bf5d6bb6cedb64b4
molly mk <mmkover@gmail.com>
2020-08-23 18:54:51 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -54,7 +54,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:" 
     holiday.each do |holiday, supplies|
-      puts "  #{holiday.to_s.split(" ").collect}: #{supplies.join(", ")}"
+      puts "  #{holiday.to_s.split(" ").collect {|split_holiday| split_holiday.capitalize.join(" ")}: #{supplies.join(", ")}"
     end
   end
 end
